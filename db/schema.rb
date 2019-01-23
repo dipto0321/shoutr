865710b7b0f7dd79fb893056016bdf7536fdf9cf
dipto0321 <diptokmk47@gmail.com>
2019-01-23 16:02:09 +0600

    Allow user to shout

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,20 +10,31 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_190_123_071_410) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+ActiveRecord::Schema.define(version: 2019_01_23_082239) do
 
-  create_table 'users', force: :cascade do |t|
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.string 'email', null: false
-    t.string 'encrypted_password', limit: 128, null: false
-    t.string 'confirmation_token', limit: 128
-    t.string 'remember_token', limit: 128, null: false
-    t.string 'username'
-    t.index ['email'], name: 'index_users_on_email'
-    t.index ['remember_token'], name: 'index_users_on_remember_token'
-    t.index ['username'], name: 'index_users_on_username', unique: true
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "shouts", force: :cascade do |t|
+    t.string "body", null: false
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_shouts_on_user_id"
   end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "email", null: false
+    t.string "encrypted_password", limit: 128, null: false
+    t.string "confirmation_token", limit: 128
+    t.string "remember_token", limit: 128, null: false
+    t.string "username"
+    t.index ["email"], name: "index_users_on_email"
+    t.index ["remember_token"], name: "index_users_on_remember_token"
+    t.index ["username"], name: "index_users_on_username", unique: true
+  end
+
+  add_foreign_key "shouts", "users"
 end
