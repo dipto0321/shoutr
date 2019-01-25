3caa9638731f5205486576ab6675fd68a56e3820
dipto0321 <diptokmk47@gmail.com>
2019-01-25 13:41:01 +0600

    Fix signup and null false for shouts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 # frozen_string_literal: true
 
 class UsersController < Clearance::UsersController
+  def new
+    @user = User.new
+  end
+
   def show
     @user = User.find(params[:id])
     @shouts = @user.shouts
