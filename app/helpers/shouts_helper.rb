5f7d7c6bd2fa1740cdaaac272db9843c9d393e70
dipto0321 <diptokmk47@gmail.com>
2019-01-31 10:30:41 +0600

    Add avater in shout helper

--- app/helpers/shouts_helper.rb
+++ app/helpers/shouts_helper.rb
@@ -1,4 +1,9 @@
 # frozen_string_literal: true
 
 module ShoutsHelper
+  def avatar(user)
+    email_digest = Digest::MD5.hexdigest(user.email)
+    gravatar_url = "//www.gravatar.com/avatar/#{email_digest}"
+    image_tag(gravatar_url)
+  end
 end
