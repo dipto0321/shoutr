664e23921c573a7e41424883e07d777794b6f888
dipto0321 <diptokmk47@gmail.com>
2019-02-01 14:33:38 +0600

    Extract timeline and dashboard classes
    Using single responsibility principle

---
+++ app/models/timeline.rb
@@ -0,0 +1,21 @@
+# frozen_string_literal: true
+
+class Timeline
+  def initialize(users, scope = Shout)
+    @users = users
+  end
+
+  def shouts
+    scope
+      .where(user_id: users)
+      .order(created_at: :desc)
+  end
+
+  def to_partial_path
+    'timelines/timeline'
+  end
+
+  private
+
+  attr_reader :users, :scope
+end
